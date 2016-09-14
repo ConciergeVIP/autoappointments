81ad64458c90efeb48d09fb43aed881718821f4d
ConciergeVIP <conciergecounselor@gmail.com>
2016-09-13 23:32:37 -0400

    Update appointment.rb

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,14 +5,14 @@ class Appointment < ActiveRecord::Base
 
   after_create :reminder
 
-  @@REMINDER_TIME = 30.minutes # minutes before appointment
+  @@REMINDER_TIME = 60.minutes # minutes before appointment
 
   # Notify our appointment attendee X minutes before the appointment time
   def reminder
     @twilio_number = ENV['TWILIO_NUMBER']
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @client = Twilio::REST::Client.new ENV['ACbc7011cf61633152e71d07700ec071fc'], ENV['102eee1cdec49d1e9380a4454e7415e0']
     time_str = ((self.time).localtime).strftime("%I:%M%p on %b. %d, %Y")
-    reminder = "Hi #{self.name}. Just a reminder that you have an appointment coming up at #{time_str}."
+    reminder = "Hi #{self.name}. A reminder about toay's appointment with Barika Grayson LMHC at 6817 Southpoint Pkwy Ste 802 Jacksonville, FL 32216 coming up at #{time_str}.  You can reach us at (904) 413-1379"
     message = @client.account.messages.create(
       :from => @twilio_number,
       :to => self.phone_number,
